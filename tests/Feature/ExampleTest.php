1d025346771dcf483f5a95ee03693eb9121983b3
BabbyBrown03 <revaldousskuncoroputra@gmail.com>
2026-09-23 09:10:06 +0700

    fix: restore passing test after pipeline verification

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,6 +14,6 @@ class ExampleTest extends TestCase
     {
         $response = $this->get('/');
 
-        $response->assertRedirect('/mahasewa');
+        $response->assertRedirect('/mahasiswa');
     }
 }
